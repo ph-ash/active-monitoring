738261d05667eae3f0809f70e845f20238e33e19
Christian Alexander Wolf <mail@cawolf.de>
2019-06-17 08:46:30 +0200

    let monitorings throw exceptions

--- src/Connector/Monitoring.php
+++ src/Connector/Monitoring.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace App\Connector;
 
 use App\Configuration\DTO\MonitoringConfiguration;
+use Exception;
 
 interface Monitoring
 {
+    /**
+     * @throws Exception
+     */
     public function execute(MonitoringConfiguration $configuration): array;
 }
